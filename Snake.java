5447b9cc0131ee5d5254fed256123351bba60461
anguslin1 <348994963@gapps.yrdsb.ca>
2022-06-02 09:48:12 -0400

    added snake

---
+++ Snake.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Snake here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Snake extends Actor
+{
+    /**
+     * Act - do whatever the Snake wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("left")){
+            setLocation(getX() - 5, getY()); 
+        }
+        if(Greenfoot.isKeyDown("right")){
+            setLocation(getX() + 5, getY()); 
+        }
+        if(Greenfoot.isKeyDown("up"))
+            setLocation(getX(), getY() - 5); 
+        if(Greenfoot.isKeyDown("down"))
+            setLocation(getX(), getY() + 5); 
+    }
+}
