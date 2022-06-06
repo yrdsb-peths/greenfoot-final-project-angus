77f0fcdabb3fa9074f6143626afd9f7d7efbb4eb
anguslin1 <348994963@gapps.yrdsb.ca>
2022-06-06 09:54:25 -0400

    added title screen

---
+++ TitleScreen.java
@@ -0,0 +1,52 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("GAME", 75);
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+
+        addObject(titleLabel, getWidth()/2, getHeight()/2);
+        prepare();
+    }
+
+    /**
+     * The main world act loop.
+     */
+    public void act()
+    {
+        // Start game if user presses space.
+        if(Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Snake snake = new Snake();
+        addObject(snake,524,52);
+        snake.setLocation(300,343);
+        Label label = new Label("Press <space> to Start", 40);
+        addObject(label,254,124);
+        label.setLocation(391,264);
+        label.setLocation(317,130);
+    }
+}
