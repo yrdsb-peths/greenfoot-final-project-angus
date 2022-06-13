b6ec2955ec2d1fa9b1412a9daf0fac064dad9f4b
anguslin1 <348994963@gapps.yrdsb.ca>
2022-06-12 21:18:29 -0400

    added apple falling

--- Apple.java
+++ Apple.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Apple here.
+ * Write a description of class apple here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
@@ -12,12 +12,19 @@ public class Apple extends Actor
      * Act - do whatever the Apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    
     public void act()
     {
         // Add your action code here.
-        setLocation(getX(), getY()); 
+        int x = getX();
+        int y = getY();
+        setLocation(x, y+2);
         
+        MyWorld world = (MyWorld) getWorld();
+        
+        if(getY() >= world.getHeight())
+        {
+            world.gameOver();
+            world.removeObject(this);
+        }
     }
-    
 }
