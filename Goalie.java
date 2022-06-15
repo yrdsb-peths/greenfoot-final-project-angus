5f7a324ed5f7fece25ab0f1dd660af084fd65b6d
anguslin1 <348994963@gapps.yrdsb.ca>
2022-06-15 10:34:22 -0400

    fixed images

--- Goalie.java
+++ Goalie.java
@@ -12,7 +12,7 @@ public class Goalie extends Actor
      * Act - do whatever the Snake wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    GreenfootImage[] idle = new GreenfootImage[10];
+    GreenfootImage[] idle = new GreenfootImage[10;
     int speed = 1;
     
     /**
